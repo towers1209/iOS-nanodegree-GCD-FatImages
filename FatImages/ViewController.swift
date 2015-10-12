e3ec110eab9d26d0d4b2fa5d41e3f742eaedcd73
Fernando Rodriguez <fernando@udacity.com>
2015-10-12 15:53:07 -0700

    Added empty stubs for the actions. This would be the first version of the project that the user would see.

--- FatImages/ViewController.swift
+++ FatImages/ViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: IBOutlets
+    @IBOutlet weak var photoView: UIImageView!
+    
+    //MARK: Actions
+    
+    // This method downloads a huge image, blocking the main queue and
+    // the UI.
+    // This si for instructional purposes only, never do this.
+    @IBAction func synchronousDownload(sender: UIBarButtonItem) {
+    }
+    
+    
+    // This method avoids blocking by creating a new queue that runs
+    // in the background, without blocking the UI.
+    @IBAction func simpleAsynchronousDownload(sender: UIBarButtonItem) {
+    }
+    
+    // This code downloads the huge image in a global queue and uses a completion
+    // closure.
+    @IBAction func asynchronousDownload(sender: UIBarButtonItem) {
+    }
+    
+    
+    // Changes the alfa value (transparency of the image). It's only purpose is to show if the
+    // UI is blocked or not.
+    @IBAction func setTransparencyOfImage(sender: UISlider) {
+        
+        photoView.alpha = CGFloat(sender.value)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
