97668cc7e9e22b46bcf91ada3c8d2c40f1e7d07c
Fernando Rodriguez <fernando@udacity.com>
2015-10-14 15:50:16 -0700

    sync download. all in one if let

--- FatImages/ViewController.swift
+++ FatImages/ViewController.swift
@@ -39,16 +39,16 @@ class ViewController: UIViewController {
     @IBAction func synchronousDownload(sender: UIBarButtonItem) {
         
         // Get the URL for the image
-        let url = NSURL(string: BigImages.seaLion.rawValue)
-        
         // Obtain the NSData with the image
-        let imgData = NSData(contentsOfURL: url!)
-        
         // Turn it into a UIImage
-        let image = UIImage(data: imgData!)
-        
-        // Display it
-        photoView.image = image
+        if let url = NSURL(string: BigImages.seaLion.rawValue),
+            let imgData = NSData(contentsOfURL: url),
+            let image = UIImage(data: imgData){
+                
+                // Display it
+                photoView.image = image
+        }
+
 
         
     }
