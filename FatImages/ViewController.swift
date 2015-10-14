5da0dfe586ebadb75f5f4871722cb661a7ae4756
Fernando Rodriguez <fernando@udacity.com>
2015-10-14 15:20:04 -0700

    used a smaller image

--- FatImages/ViewController.swift
+++ FatImages/ViewController.swift
@@ -20,7 +20,7 @@ import UIKit
 // networking section.
 // At this point it's not relevant.
 enum BigImages: String{
-    case sharkAttack = "https://lh3.googleusercontent.com/ZGuyQQYYhQM0rOVNGjUwKSAui0FSzqto3CLFPsqxmtcIAhiAcxmtHpF3I_To7k3aGI8Yz1Gijj4g4Aot075Q=s0#w=5100&h=3839"
+    case whale = "https://lh3.googleusercontent.com/16zRJrj3ae3G4kCDO9CeTHj_dyhCvQsUDU0VF0nZqHPGueg9A9ykdXTc6ds0TkgoE1eaNW-SLKlVrwDDZPE=s0#w=4800&h=3567"
     case shark = "https://lh3.googleusercontent.com/BCoVLCGTcWErtKbD9Nx7vNKlQ0R3RDsBpOa8iA70mGW2XcC76jKS09pDX_Rad6rjyXQCxngEYi3Sy3uJgd99=s0#w=4713&h=3846"
     case seaLion   = "https://lh3.googleusercontent.com/ibcT9pm_NEdh9jDiKnq0NGuV2yrl5UkVxu-7LbhMjnzhD84mC6hfaNlb-Ht0phXKH4TtLxi12zheyNEezA=s0#w=4626&h=3701"
 }
