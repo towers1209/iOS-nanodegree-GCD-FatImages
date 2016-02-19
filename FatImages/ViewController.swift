3376a59543f8705ccd0889efab9eaf3cf641d96a
Loreto E Torres <Loreto.E.Torres@gmail.com>
2016-02-18 19:56:31 -0500

    Added the following comments in ViewController.swift:     Branch Step5.1 code     Code for synchronousDownload updated so that the app activity indicator works.
    RATIONALE: Creating a pull request to simply update the branch label does not work unless a file
    is also modified.
    
    Also, make correction in withBigImage() so that the completion handler executes in the main queue
    instead of the global queue QOS_CLASS_USER_INITIATED.

--- FatImages/ViewController.swift
+++ FatImages/ViewController.swift
@@ -5,6 +5,10 @@
 //  Created by Fernando Rodriguez on 10/12/15.
 //  Copyright © 2015 Udacity. All rights reserved.
 //
+/*
+    Branch Step5.1 code
+    Code for synchronousDownload updated so that the app activity indicator works.
+*/
 
 import UIKit
 
@@ -147,7 +151,7 @@ class ViewController: UIViewController {
                     
                     // run the completion block
                     // always in the main queue, just in case!
-                    dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), { () -> Void in
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
                         handler(image: img)
                         
                         
